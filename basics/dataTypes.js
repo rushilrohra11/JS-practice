c8e282084a2e8aa46b392afb98bc8efcad3f9d14
Rushil Rohra <65966841+rushilrohra11@users.noreply.github.com>
2024-11-28 15:17:58 +0000

    datatypes done

---
+++ basics/dataTypes.js
@@ -0,0 +1,23 @@
+// alert("hello")
+// using node js, and not browser
+
+console.log(3+3)
+
+let name = "Rushil"
+let age = 19
+let isLoggedIn = false
+
+/*
+number - 2^53 range
+bigint - better range than number
+
+string - double quotes
+
+boolean - true or false
+null - standalone value
+undefined - no value is assigned
+
+symbol - unique
+
+object - 
+*/
